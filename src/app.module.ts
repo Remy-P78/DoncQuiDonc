29fb393e8d877cb77d1ce139a5024237dcd080f6
Remy-P78 <remy80.dev@gmail.com>
2023-10-10 16:38:43 +0200

    1.0 package nest et config main

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: [`.env`] }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: false,
+      logging: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule); // créer une instance d'application Nest
+  app.useGlobalPipes(new ValidationPipe()); // au moment où les données arrivent du front et avant de traiter une requête
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4200', // Domaine que vous souhaitez autoriser
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // autorise requete avec token etc..
+  };
+
+  app.enableCors(corsOptions); // Mise en place de Cors selon les options
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder() // Mise en place de Swagger
+    .setTitle('DoncQuiDonc example')
+    .setDescription('DoncQuiDonc description')
+    .setVersion('1.0')
+    .addTag('quiz')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
