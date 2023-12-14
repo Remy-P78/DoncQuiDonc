d708d9f8253c37a86afcf636e706370f349317fe
Remy-P78 <remy80.dev@gmail.com>
2023-12-14 16:17:37 +0100

    9.2 cors origin https

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule); // créer une instance d'application Nest
   app.useGlobalPipes(new ValidationPipe()); // au moment où les données arrivent du front et avant de traiter une requête
   const corsOptions: CorsOptions = {
-    // origin: 'http://localhost:4200', // Domaine que vous souhaitez autoriser
-    origin: '*', // Domaine que vous souhaitez autoriser
+    origin: 'http://localhost:4200, https://doncquidonc.fr', // Domaine que vous souhaitez autoriser
+    // origin: '*', // Domaine que vous souhaitez autoriser
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // autorise requete avec token etc..
-    allowedHeaders: 'Origin, Content-Type, X-Auth-Token', // Ajoutez les en-têtes que vous autorisez
+    // allowedHeaders: 'Origin, Content-Type, X-Auth-Token', // Ajoutez les en-têtes que vous autorisez
   };
 
   app.enableCors(corsOptions); // Mise en place de Cors selon les options
